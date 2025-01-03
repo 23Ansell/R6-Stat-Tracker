adcf00c97136b2c2ec4f85785946a57d3b7bb1ef
23Ansell <23AnsellS689@collyers.ac.uk>
2025-01-03 18:33:50 +0000

    Set up the layout for the JSON file

--- python/tracking.py
+++ python/tracking.py
@@ -4,15 +4,19 @@ import discord
 import os
 from dotenv import load_dotenv
 
-load_dotenv("details/.env")
+load_dotenv(".env")
 
 async def run():
     auth = Auth(os.getenv('EMAIL'), os.getenv('PASSWORD'))
 
-    player = await auth.get_player(name="CNDRD")
+    player = await auth.get_player(name="That9464")
 
     print(f"Name: {player.name}")
     print(f"Profile pic URL: {player.profile_pic_url}")
+
+    await player.load_ranked_v2()
+    print(f"Ranked Points: {player.ranked_profile.rank_points}")
+    print(f"Rank: {player.ranked_profile.rank}")
 
     await auth.close()
     
